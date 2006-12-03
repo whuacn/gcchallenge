81b18c1e7445f260741cb831706367160a7c55e1
to.undead <to.undead@7e98a58a-e119-0410-9a6b-a16cffc36142>
2006-12-03 09:04:49 +0000

    User management tools removed Access Control removed

--- trunk/src/DataProvider/IDataProvider.cs
+++ trunk/src/DataProvider/IDataProvider.cs
@@ -1,5 +1,6 @@
 using System;
 using GmatClubTest.Data;
+using System.Data;
 
 namespace GmatClubTest.DataProvider
 {
@@ -8,10 +9,11 @@ namespace GmatClubTest.DataProvider
       void Open();
       void Close();
       
-      void GetUsers(UserSet users);
+      /*void GetUsers(UserSet users);
       void Update(UserSet users);
       void GetUser(int id, UserSet user);
-      
+        */
+        
       void GetTests(TestSet tests);
       void GetQuestionSetsByTestId(int testId, QuestionSetSet questionSets);
       void GetQuestionsAnswersByQuestionSetId(int questionSetId, QuestionAnswerSet questionsAnswers);
@@ -25,6 +27,10 @@ namespace GmatClubTest.DataProvider
         
       void GetResults(int userId, DateTime beginTime, DateTime endTime, ResultSet  results);
       void GetQuestionSetsResultDetailsSet(int resultId, QuestionSetsResultDetailsSet questionSetsResultsDetailsEx);
+      
 
-          }
+      IDbConnection getConnection();
+
+
+   }
 }
