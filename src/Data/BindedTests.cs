62b00e5970718ca111e30d1a4990aa8537eb5593
to.undead <to.undead@7e98a58a-e119-0410-9a6b-a16cffc36142>
2006-12-03 09:38:50 +0000

    BindedTests added

---
+++ src/Data/BindedTests.cs
@@ -0,0 +1,12 @@
+﻿namespace GmatClubTest.Data.BindedTestsTableAdapters 
+{
+   public partial class binded_testsTableAdapter
+   {
+      public System.Data.SqlClient.SqlConnection SqlConnection
+      {
+         get { return this.Connection; }
+         set { this.Connection = value; }
+      }
+   };
+}
+
