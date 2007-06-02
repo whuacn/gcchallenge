a421b25a2f2d5e0ed246565f355c1dba69283af5
yuriy.stasiv <yuriy.stasiv@7e98a58a-e119-0410-9a6b-a16cffc36142>
2007-06-02 09:24:06 +0000

    Added sql script for create Explanation table

--- trunk/src/SqlScripts/AddExplanationColumn.sql
+++
@@ -1,8 +0,0 @@
-if exists (Select 1 from sys.columns
-inner join sys.objects on sys.columns.object_id = sys.objects.object_id
-where sys.columns.name = 'explanation' and sys.objects.name = 'Answers')
-begin
-	Alter table [Answers] drop column explanation
-end
-
-Alter table [Answers] add [explanation] varchar(max) null
